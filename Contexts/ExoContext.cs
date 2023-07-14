cbf04974a8d3c18c498353f128bf85a8694140d0
Alberico-Marques <alberico_marques@hotmail.com>
2023-07-14 00:15:32 -0300

    CRUD de usuário

--- Contexts/ExoContext.cs
+++ Contexts/ExoContext.cs
@@ -5,7 +5,6 @@ using Microsoft.Data.SqlClient;
 
 namespace Exo.WebApi.Contexts
 {
-<<<<<<< HEAD
     public class ExoContext : DbContext
     {
         public ExoContext()
@@ -22,7 +21,7 @@ namespace Exo.WebApi.Contexts
             if(!optionsBuilder.IsConfigured)
             {
             // Essa string de conexão depende da SUA máquina.
-            optionsBuilder.UseSqlServer("Server=Andrey\\SQLEXPRESS;"+ "Database=ExoApi;User ID=sa;Password=12345;Trusted_Connection=False;");
+            optionsBuilder.UseSqlServer("Server=Andrey\\SQLEXPRESS;"+ "Database=ExoApi;User ID=sa;Password=12345;Trusted_Connection=True;");
 
             //("Server=Andrey\\SQLEXPRESS;"+ "Database=ExoApi;User ID=sa;Password=12345;Trusted_Connection=True;");
             // Exemplo 1 de string de conexão:
@@ -34,35 +33,8 @@ namespace Exo.WebApi.Contexts
             }
         }
         public DbSet<Projeto> Projetos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+       
+  
     }
-=======
-public class ExoContext : DbContext
-{
-public ExoContext()
-{ 
-}
-public ExoContext(DbContextOptions<ExoContext> options) : 
-base(options)
-{
-}
-protected override void OnConfiguring(DbContextOptionsBuilder
-optionsBuilder)
-{
-if(!optionsBuilder.IsConfigured)
-{
-// Essa string de conexão depende da SUA máquina.
-optionsBuilder.UseSqlServer("Server=ANDREY\\SQLEXPRESS;"
-+ "Database=ExoApi;User ID=sa;Password=12345;Trusted_Connection=False;");
-// Exemplo 1 de string de conexão:
-// User
-//ID=sa;Password=Abc12345;Server=localhost;Database=ExoApi;-
-// + Trusted_Connection=False;
-// Exemplo 2 de string de conexão:
-// 
-//Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=True;
-}
-}
-public DbSet<Projeto> Projetos { get; set; }
-}
->>>>>>> 7fd1bb56e36a51f88e7d7da797e8eaf4b42be3c3
 }
